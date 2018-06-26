575ceb844703a5a60445d5629e377d8d138fa432
Mthokozisi <mthokozisimahlangu0@gmail.com>
2018-06-26 12:54:56 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,42 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  scoreTeamA = 0;
+  scoreTeamB = 0;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  onReset(){
+    this.scoreTeamA = 0;
+    this.scoreTeamB = 0;
+  }
+  
+  onClickA(){
+    this.scoreTeamA += 3;
+    console.log(this.scoreTeamA);
+  }
+
+  onTwoA(){
+    this.scoreTeamA += 2;
+    console.log(this.scoreTeamA);
+  }
+
+  onThrowA(){
+    this.scoreTeamA += 1;
+    console.log(this.scoreTeamA);
+  }
+  onClickB(){
+    this.scoreTeamB += 3;
+    console.log(this.scoreTeamB);
+  }
+  onTwoB(){
+    this.scoreTeamB += 3;
+    console.log(this.scoreTeamB);
+  }
+  onThrowB(){
+    this.scoreTeamB += 3;
+    console.log(this.scoreTeamB);
+  }
 }
